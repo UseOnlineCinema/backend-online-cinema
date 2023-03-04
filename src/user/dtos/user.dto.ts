04831e61b7b0931c3b6901e4ade552e0d0996740
Giovane <giovaneaalmeida27@gmail.com>
2023-03-04 15:10:16 -0300

    feat: add swagger to user dto

--- src/user/dtos/user.dto.ts
+++ src/user/dtos/user.dto.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserDto {
+  @ApiProperty({
+    description: "the user's id",
+    example: '59902b33-3bf2-4cf8-ac75-c8a53a6c6e71',
+  })
   id: string;
+
+  @ApiProperty({
+    description: "the user's username",
+    example: 'josvane',
+  })
   username: string;
+
+  @ApiProperty({
+    description: "the user's email",
+    example: 'giovaneaalmeida27@gmail.com',
+  })
   email: string;
 }
