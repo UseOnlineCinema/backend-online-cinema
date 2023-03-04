38badeb7aea434929013ffa0737cc751e3b86bb7
Giovane <giovaneaalmeida27@gmail.com>
2023-03-04 13:13:52 -0300

    feat: create user service and handle function

--- src/user/uses-cases/sign-up/sign-up.service.ts
+++ src/user/uses-cases/sign-up/sign-up.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { Prisma } from '@prisma/client';
+import { genSaltSync, hash } from 'bcrypt';
+import { UserDto } from 'src/user/dtos/user.dto';
+import * as crypto from 'crypto';
 
 @Injectable()
-export class SignUpService {}
+export class SignUpService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async handle(signUpUserDto: Prisma.UserCreateInput): Promise<UserDto> {
+    if (
+      !signUpUserDto.email ||
+      !signUpUserDto.username ||
+      !signUpUserDto.password
+    ) {
+      throw new BadRequestException(
+        'Please insert username, email and password',
+      );
+    }
+
+    const foundUser = await this.prismaService.user.findFirst({
+      where: {
+        OR: [
+          {
+            email: signUpUserDto.email,
+          },
+          {
+            username: signUpUserDto.username,
+          },
+        ],
+      },
+    });
+
+    if (foundUser) {
+      throw new ConflictException('The username or email is already in use');
+    }
+
+    const salt = genSaltSync(8);
+    const hashedPassword = await hash(signUpUserDto.password, salt);
+
+    const user = {
+      id: crypto.randomUUID(),
+      password: hashedPassword,
+      email: signUpUserDto.email,
+      username: signUpUserDto.username,
+    };
+
+    await this.prismaService.user.create({
+      data: user,
+    });
+
+    return {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+  }
+}
