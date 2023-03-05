16c1a7b6aeee27b96b04d0c10f5bdd8bf47426b4
Giovane <giovaneaalmeida27@gmail.com>
2023-03-05 11:51:55 -0300

    feat: implements authorization flow

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { PrismaService } from 'src/prisma.service';
+import { AuthService } from './auth.service';
+
+@Module({
+  providers: [AuthService, PrismaService],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
