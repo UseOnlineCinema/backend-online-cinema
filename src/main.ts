0a1c35c5f66b810a5271e426c65a318b460cc407
Giovane <giovaneaalmeida27@gmail.com>
2023-03-07 20:11:32 -0300

    feat: add aditional swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,18 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Online Cinema API')
     .setDescription('The API of the project Online Cinema')
+    .addBearerAuth()
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      defaultModelsExpandDepth: 0, // models closed at first
+      docExpansion: 'none', // hide the models
+    },
+  });
 
   await app.listen(3000);
 }
