2421f12ce15b73f493dffea64124d0a71810b637
Giovane <giovaneaalmeida27@gmail.com>
2023-03-07 15:34:52 -0300

    feat: create admin auth guard

---
+++ src/auth/admin-auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    if (!request.user.role.includes('ADMIN')) {
+      throw new ForbiddenException(
+        'You do not have the rights to access this resource',
+      );
+    }
+
+    return true;
+  }
+}
