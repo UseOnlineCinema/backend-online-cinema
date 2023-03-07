b706eb067dc7b7f274146bf4266adbb865daf4b8
Giovane <giovaneaalmeida27@gmail.com>
2023-03-07 14:09:42 -0300

    feat: create movie module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, MovieModule],
   controllers: [],
   providers: [],
 })
