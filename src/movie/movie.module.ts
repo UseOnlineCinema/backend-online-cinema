e2695953f624379dfad40ac7f137a76c6d752a98
Giovane <giovaneaalmeida27@gmail.com>
2023-03-11 19:13:03 -0300

    feat: create movie module

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CreateMovieService } from './use-cases/create-movie/create-movie.service';
 import { CreateMovieController } from './use-cases/create-movie/create-movie.controller';
+import { UpdateMovieService } from './use-cases/update-movie/update-movie.service';
+import { UpdateMovieController } from './use-cases/update-movie/update-movie.controller';
 
 @Module({
-  providers: [CreateMovieService, PrismaService],
-  controllers: [CreateMovieController],
+  providers: [CreateMovieService, PrismaService, UpdateMovieService],
+  controllers: [CreateMovieController, UpdateMovieController],
 })
 export class MovieModule {}
