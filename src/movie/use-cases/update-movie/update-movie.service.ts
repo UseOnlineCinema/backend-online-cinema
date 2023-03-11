2a5a6ad72780e2a393643d02395c0892e7218cf9
Giovane <giovaneaalmeida27@gmail.com>
2023-03-11 19:12:45 -0300

    feat: create update movie service

---
+++ src/movie/use-cases/update-movie/update-movie.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { UpdateMovieDto } from './dtos/update-movie.dto';
+
+@Injectable()
+export class UpdateMovieService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async handle(id: string, updateMovieDto: UpdateMovieDto): Promise<Movie> {
+    const foundMovie = await this.prismaService.movie.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!foundMovie) {
+      throw new NotFoundException('No movie with that id was found');
+    }
+
+    return this.prismaService.movie.update({
+      where: {
+        id,
+      },
+      data: updateMovieDto,
+    });
+  }
+}
