b35ba229c559f104ef29a5f5b19d1f6428767493
Giovane <giovaneaalmeida27@gmail.com>
2023-03-20 22:02:59 -0300

    feat: create movie data provider

---
+++ test/shared/movie.ts
@@ -0,0 +1,13 @@
+export abstract class MovieDataProvider {
+  getMockedMovie() {
+    return {
+      id: '27a7bd1a-c42e-46d9-9b04-f023e0ff34ce',
+      name: 'The Matrix',
+      synopsis:
+        'The Matrix is a 1999 science fiction action film written and directed by the Wachowskis. It is the first installment in the Matrix film series, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and depicts a dystopian future in which humanity is unknowingly trapped inside the Matrix, a simulated reality that intelligent machines have created to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias "Neo", uncovers the truth, he joins a rebellion against the machines along with other people who have been freed from the Matrix',
+      duration: 9780,
+      cast: 'Keanu Reeves, Carrie-Anne Moss, Laurence Fishburne, Hugo Weaving, Joe Pantoliano, Marcus Chong...',
+      url: 'https://www.youtube.com/watch?v=2gliGzb2_1I',
+    };
+  }
+}
