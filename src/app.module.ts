9a036c325812cbd2349a9caea9ff0b4b9bbeb4de
jeversonjv <jeversontp@gmail.com>
2023-03-30 11:01:33 -0300

    feat: sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MovieModule } from './movie/movie.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UserModule, AuthModule, MovieModule, PrismaModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as unknown as number,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+    }),
+    UserModule,
+    AuthModule,
+    MovieModule,
+  ],
   controllers: [],
   providers: [],
 })
